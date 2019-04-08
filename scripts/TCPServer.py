602cbe716c2ecd7c7e54dd66d02700c34cb94447
user <you@example.com>
2019-04-08 15:38:09 -0300

    Server TCP para ler poesia

--- scripts/TCPServer.py
+++ scripts/TCPServer.py
@@ -12,18 +12,63 @@
 
 import socket as sk
 
-serverPort = 12002
+serverPort = 12000
 serverSocket = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
 serverSocket.bind(('', serverPort))
 serverSocket.listen(10)
 
 print("Servidor pronto para receber")
 
+def page(port):
+    string = '''
+    <html>
+    <head>
+        <title>Python is awesome!</title>
+    </head>
+    <body>
+        <h1>Usando redes para ler poesia</h1>
+        <h2>DIGITE: SERVER_IP:%s/poesia1.txt</h2>
+        <h2>or</h2>
+        <h2>DIGITE: SERVER_IP:%s/poesia2.txt</h2>
+        <p>e bom proveito!</p>
+    </body>
+    </html>'''%(port, port)
+    return string.encode('utf-8')
+
 while True:
-	connectionSocket , addr = serverSocket.accept()
-	message = connectionSocket.recv(1024)
-	if(message):
-		print message
-	modifiedMessage = message.decode('utf-8').upper()
-	connectionSocket.send(modifiedMessage.encode('utf-8'))
-	connectionSocket.close()
+    connectionSocket , addr = serverSocket.accept()
+    message = connectionSocket.recv(1024)
+    
+    if(message):
+        print()
+        commandList = message.decode().split()
+        print(commandList)
+        if('GET' in commandList or 'get' in commandList):
+            #print("tem get aqui")
+            fileName = commandList[1][1:]
+            if(fileName.endswith(".txt")):
+                print("Requisição do arquivo %s"%fileName)
+     
+                try:
+                    file = open(fileName, 'r')
+                    print("Conteúdo do arquivo")
+                    connectionSocket.send("HTTP/1.1 200 OK\n\r".encode('utf-8'))
+         
+                    for line in file.readlines():
+                        print(line)
+                        connectionSocket.send(line.encode('utf-8'))
+                except IOError:
+                    connectionSocket.send("HTTP/1.1 404 Not Found\n\r".encode('utf-8'))
+                    print("\n\n404 Not Found\n\n")
+                    connectionSocket.send("404 Not Found".encode('utf-8'))
+            else:
+                connectionSocket.send("HTTP/1.1 200 OK\n\r".encode('utf-8'))
+                connectionSocket.send(page(serverPort))
+    else:
+        try:
+            connectionSocket.send("HTTP/1.1 200 OK\n\r".encode('utf-8'))
+            connectionSocket.send(page(serverPort))
+        except:
+            pass
+    
+    connectionSocket.close()
